4bf0a81a14023759a380a03b03e0907c2d62cfd9
nswarnalatha <nswarnalatha@nswarnalatha-lp.pkrmsezdc.com>
2018-12-20 20:21:56 +0530

    first commit into new repo

---
+++ src/main/java/com/pk/zomato/ZomatoJavaIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.pk.zomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoJavaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoJavaIntegrationApplication.class, args);
+	}
+
+}
+
